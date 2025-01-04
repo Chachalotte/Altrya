8347d5be19829c49894a2c0544a0033c0331704c
Charlotte <petittofu60@gmail.com>
2025-01-04 21:55:47 +0100

    Added first components, primeng for style handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, PanelModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'altrya';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { FrontPageComponent } from './components/front-page/front-page.component';
+import { CollectionsComponent } from './components/collections/collections.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'front-page', component: FrontPageComponent
+    },
+    {
+        path: 'collections', component: CollectionsComponent
+    }
+];
